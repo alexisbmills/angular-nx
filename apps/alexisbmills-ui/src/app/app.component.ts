e08eff424198a6ddf2fc7b930d61991f9d6936c0
alexisbmills <alexis.bradleymills@gmail.com>
2021-05-04 13:53:02 +0800

    chore(build): replace with protractor and karma

---
+++ apps/alexisbmills-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-nx-abm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'alexisbmills-ui';
+}
